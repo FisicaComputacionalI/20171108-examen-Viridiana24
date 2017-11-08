507fa0b9757c4eec480f96e28a94927f50b3cf9e
Viridiana <31377504+Viridiana24@users.noreply.github.com>
2017-11-08 13:56:43 -0600

    Add files via upload

---
+++ examenp2.cc
@@ -0,0 +1,31 @@
+//EXAMEN
+//Barranco Diaz Carmen Viridiana 
+//08/11/17
+
+//El programa te pide un numero enterote pregunta si es igual o menor que 5 si no es asi paraas el programa si no continuas con una operacion que te pide.
+
+
+//iniciamos el programa 
+#include<iostream>
+using namespace std;
+//definimos una variable 
+int main(){
+	int X;
+
+//Damos un numero arbitario 
+cout<<"Dame un numero entero"<<endl;
+cin>>X;
+
+//tomamos valores desde 1
+for(int i=1; i++)	
+
+if(X<=5)
+
+if((X%5)==0){
+X=X+1;
+
+if(X<=5)
+
+return 0;
+}
+
