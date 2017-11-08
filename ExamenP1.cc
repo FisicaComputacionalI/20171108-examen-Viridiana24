4c9492bd35abf3c93c25d55fdf76cda544ab9fcf
Viridiana <31377504+Viridiana24@users.noreply.github.com>
2017-11-08 13:39:18 -0600

    Add files via upload

---
+++ ExamenP1.cc
@@ -0,0 +1,54 @@
+//Programa 1
+//Barranco Diaz Carmen Viridiana
+//08 noviembre 2017
+
+#include <iostream>
+using namespace std;
+//int main estaba en lugar incorrecto 
+//las variables no estan correctamente declaradas 
+
+int main (){
+  int S=12;
+  int p=0;
+  int flag=0;
+
+int isPrime(int X)
+  int flag=0; 
+ 
+  for(int i=2; i<= (X/2) ; i++)
+{
+//la condicion in no estaba en parentesis
+    if((X%i)==0){
+      flag=1;
+      break;
+    }
+  }    
+  cout << "Dame un número entero" << endl;
+  cin >> p;
+
+ while (S!=0){ 
+   cout << "ciclo S!=0, S " << S << ",p " << p << endl;
+   while(p<S){
+       cout << "ciclo p<S, S " < S << ",p " << p << endl;
+
+    while(isPrime(p)==1){
+      cout << "ciclo isPrime, S " << S << ",p " << p << endl;
+      cout << p << " no es primo  " << endl;
+      if((p%2)==0)
+	p=p+1;
+      p=p+2;
+      cout << "final ciclo isPrime, S " << S << ",p " << p << endl;
+    }  
+    
+    S=S-p;
+    p=p+2
+    cout << "final ciclo p<S, S " << S << ",p " << p << endl;
+   }
+
+  S=S-1;
+  cout < "final ciclo S!=0, S " << S << ",p " << p << endl;
+ }
+  
+  cout << " el valor de P es " << p << endl;
+  return 0;
+}
