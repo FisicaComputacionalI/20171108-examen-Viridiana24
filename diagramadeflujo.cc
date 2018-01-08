d184c7d835e8d27d08bf3ee8988aee92f558e19e
Viridiana <31377504+Viridiana24@users.noreply.github.com>
2018-01-08 14:14:27 -0600

    Add files via upload

---
+++ diagramadeflujo.cc
@@ -0,0 +1,22 @@
+//Barranco Diaz Carmen Viridiana
+//Traducir diagrama de flujo a lenguaje c
+//el programa comienza con x=1 si 1<=5 entonces ingresarà al ciclo While Cuando x sea mayor que 5 (x>5 )el programa termina.
+//luego se ve si x  entre 3, si no lo es entonces el programa imprime el número en la terminal y si es divisible no lo imprime, luego hacemos x=x+2 y vemos si se cumple la condiciòn para que entre o no al ciclo While,
+//repitiendo todo lo anterior. El programa llega a un final dado se va sumando 2 a x cuando  cumpla la condidión terminarà el programa.
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+	int x=1;
+	while(x<=5){
+		if(x%3==0){
+			x=x+2;
+		}
+		else{
+			cout<<x<<endl;
+			x=x+2;
+		}
+	}
+	return 0;
+}
---
+++ veloscorregido.cc
@@ -0,0 +1,16 @@
+//Barranco Diaz Carmen Viridiana 
+//Programa de la velocidad 
+#include <iostream>
+using namespace std;
+int main (){
+	double d=0;
+	double t=0;
+	double v=0;
+	cout<<"Dame la distancia"<<endl;
+	cin >>d;
+	cout<<"Dame el tiempo" <<endl;
+	cin>>t;
+	v=d/t;
+	cout<<"La velocidad es igual a"<<v<<endl;
+	return 0;
+}
